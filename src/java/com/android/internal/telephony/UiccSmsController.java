8601b80656a7ce92e306cc48c1754364c455a30d
artas182x <artas182x@gmail.com>
2014-11-30 18:39:47 +0100

    Fix typo

--- src/java/com/android/internal/telephony/UiccSmsController.java
+++ src/java/com/android/internal/telephony/UiccSmsController.java
@@ -130,7 +130,7 @@ public void sendDataWithOrigPort(String callingPackage, String destAddr, String 
                  scAddr, destPort, origPort, data, sentIntent, deliveryIntent);
     }
 
-    ppublic void sendDataWithOrigPortUsingSubscriber(long subId, String callingPackage,
+    public void sendDataWithOrigPortUsingSubscriber(long subId, String callingPackage,
             String destAddr, String scAddr, int destPort, int origPort, byte[] data,
             PendingIntent sentIntent, PendingIntent deliveryIntent) {
         IccSmsInterfaceManager iccSmsIntMgr = getIccSmsInterfaceManager(subId);
